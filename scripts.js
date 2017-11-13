0a5bd98e78d1a60c8343cad235216ec28e0cf59d
Tyler Bissmeyer <tylerbissmeyer@Tyler-Bissmeyers-MacBook-Pro.local>
2017-11-13 10:02:49 -0500

    Resized Images

--- scripts.js
+++ scripts.js
@@ -35,12 +35,13 @@ VALIDATE FORM
 
 
 
-
+/* Store the array values in a variable */
 function validateForm(formsCheck) {
   var formFields = ["user_name", "user_email", "user_phone", "user_subject", "user_message"];
-  
-  var errors = "";
 
+  /* If variable is empty run the loop */
+  var errors = "";
+  /* Check each user field to see if left blank, replace with a concatanated alert message telling the user which field is left blank. */
   for (i = 0; i < formFields.length; i++) { 
       var x = document.forms["myForm"][formFields[i]].value;
       if (x == "") {
@@ -57,7 +58,7 @@ function validateForm(formsCheck) {
   
 
 }
-// debugger;
+
 
 
 
